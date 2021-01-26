17d6d770ffdfc300d2e948d06cd6dc2b5dff35b9
miner90027 <alekszander.mccormick@outlook.com>
2021-01-26 12:01:07 -0900

    -added all of the required functions from Lab 2 Create a local variable of an object of your class Create a local variable that is a copy of another object of your class Pass an object by value Pass an object by reference Pass an object by reference to const Return an object by value Create a vector of 5 objects

--- main.cpp
+++ main.cpp
@@ -1,6 +1,71 @@
+/* Aleks McCormick
+ * UAF CS 202
+ * Lab 3
+ * 01/26/2021
+ */
 #include <iostream>
+#include <string>
+#include <vector>
+#include "Monster.hpp"
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+//begin Lab 2 functions
+void pasByVal(Monster foo){
+	cout << "begin pasByVal function\n";
+	cout<< "the Monster is a "<< foo.getSpecies()<<endl;
+	foo.setSpecies("Human");
+	cout<< "changed foo to" << foo.getSpecies()<<endl;
+	cout<<"end of pasByVal function\n";
+}
+void pasByRef(Monster &faa){
+	cout << "begin pasByRef function\n";
+	cout<< "the Monster is a "<< faa.getSpecies()<<endl;
+	faa.setSpecies("Human");
+	cout<< "changed foo to" << faa.getSpecies()<<endl;
+	cout<<"end of pasByRef function\n";
+}
+void pasByRefConst(Monster const bar){
+	cout << "begin pasByRefConst function\n";
+	cout<< "the Monster is a "<< bar.getSpecies()<<endl;
+	cout<<"end of pasByRefConst function\n";
+}
+Monster retrnByVal(){
+	cout << "begin retrnByVal function\n";
+	Monster biz("slime");
+	cout<<"end of retrnByVal function\n";
+	return biz;
+}
+//end Lab 2 functions
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	//begin lab 2 stuff
+	cout<<"create local variable of object\n";
+	Monster deflt;
+
+	cout<<"create a copy of an object\n";
+	Monster copyDeflt(deflt);
+
+	cout<<"create object with parameters\n";
+	Monster("Goblin");
+
+	cout<<"pass object by value\n";
+	pasByVal(deflt);
+
+	cout<<"pass object by reff\n";
+	pasByRef(deflt);
+
+	cout<<"pass object by reff to const\n";
+	pasByRefConst(copyDeflt);
+
+	cout<<"return an object by value\n";
+	Monster val = retrnByVal();
+	cout<< "the returned monster is a "<< val.getSpecies()<< endl;
+
+	cout<<"create a vector of 5 objects\n";
+	vector<Monster> vec(5);
+
+	//end Lab 2 stuff
 }
