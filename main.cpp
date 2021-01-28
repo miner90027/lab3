fb9750fbcbb9708d58bb77c7d9b635ba259be2a1
miner90027 <alekszander.mccormick@outlook.com>
2021-01-28 11:04:31 -0900

    - deleted functions and stuff from Lab 2 - added the stuff for Lab 3 demonstrating smart pointers

--- main.cpp
+++ main.cpp
@@ -7,70 +7,41 @@
 #include <string>
 #include <vector>
 #include "Monster.hpp"
+#include <memory>
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+using std::unique_ptr;
+using std::make_unique;
+using std::shared_ptr;
+using std::make_shared;
+using std::move;
 
-//begin Lab 2 functions
-/*
-void pasByVal(Monster foo){
-	cout << "begin pasByVal function\n";
-	cout<< "the Monster is a "<< foo.getSpecies()<<endl;
-	foo.setSpecies("Human");
-	cout<< "changed foo to " << foo.getSpecies()<<endl;
-	cout<<"end of pasByVal function\n";
-}
-void pasByRef(Monster &faa){
-	cout << "begin pasByRef function\n";
-	cout<< "the Monster is a "<< faa.getSpecies()<<endl;
-	faa.setSpecies("Human");
-	cout<< "changed foo to" << faa.getSpecies()<<endl;
-	cout<<"end of pasByRef function\n";
-}
-void pasByRefConst(Monster const bar){
-	cout << "begin pasByRefConst function\n";
-	cout<< "the Monster is a "<< bar.getSpecies()<<endl;
-	cout<<"end of pasByRefConst function\n";
-}
-Monster retrnByVal(){
-	cout << "begin retrnByVal function\n";
-	Monster biz("slime");
-	cout<<"end of retrnByVal function\n";
-	return biz;
-}
- */
-//end Lab 2 functions
+
 
 int main() {
-	//begin lab 2 stuff
-	/*
-	cout<<"create local variable of object\n";
-	Monster deflt;
+	cout<<"Creating a raw pointer\n";
+	Monster *dragon = new Monster;
+	cout<<endl;
 
-	cout<<"create a copy of an object\n";
-	Monster copyDeflt(deflt);
+	cout<<"Creating a unique_ptr\n";
+	unique_ptr<Monster> gnome = make_unique<Monster>("gnome");
+	cout<<endl;
 
-	cout<<"create object with parameters\n";
-	Monster("Goblin");
+	cout<<"Transfering ownership of unique_ptr\n";
+	unique_ptr<Monster> evolvedGnome = move(gnome);
 
-	cout<<"pass object by value\n";
-	pasByVal(deflt);
 
-	cout<<"pass object by reff\n";
-	pasByRef(deflt);
+	cout<<"Change the species of the new unique_ptr using a member function\n";
+	evolvedGnome -> setSpecies("Gremlin");
+	cout<<evolvedGnome -> getSpecies();
+	cout<<endl<<endl;
 
-	cout<<"pass object by reff to const\n";
-	pasByRefConst(copyDeflt);
-
-	cout<<"return an object by value\n";
-	Monster val = retrnByVal();
-	cout<< "the returned monster is a "<< val.getSpecies()<< endl;
-
-	cout<<"create a vector of 5 objects\n";
-	vector<Monster> vec(5);
-	*/
-	//end Lab 2 stuff
-	//Begin Lab 3 stuff
-
+	cout<<"Creating a shared_ptr\n";
+	shared_ptr<Monster> bees = make_shared<Monster>("bees");
+	{
+		cout<<"Creating another shared_ptr pointing at the same object in a diffrent scope\n";
+		shared_ptr<Monster> hive = bees;
+	}
 }
